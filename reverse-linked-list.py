e59c03f0bad78c9cb1db86f2fb0ac29009c8474e
zhuo.meng <mengzhuo1203@gmail.com>
2015-06-15 22:44:30 +0800

    add rll

---
+++ reverse-linked-list.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        last, current = None, head
+        while current:
+            next = current.next
+            current.next = last
+            last = current
+            current = next
+        return last
