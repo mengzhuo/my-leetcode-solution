48b7d730c35dd4b937714dd53b4ad8bbf6e5bc76
zhuo.meng <mengzhuo1203@gmail.com>
2015-06-25 22:51:31 +0800

    add reverse bits

---
+++ reverse-bits.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        return int(bin(n)[2:].zfill(32)[::-1], 2)
